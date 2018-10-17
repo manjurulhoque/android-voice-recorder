9dfdd33ba6847b31c9ecab6a5c4997adfb7af39b
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-10-17 19:18:30 +0600

    Check for permission and Adapter added

--- app/src/main/java/com/manjurulhoque/voicerecorder/MainActivity.java
+++ app/src/main/java/com/manjurulhoque/voicerecorder/MainActivity.java
@@ -1,5 +1,8 @@
 package com.manjurulhoque.voicerecorder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+    }
+
+    private void checkPermission() {
+        String[] PERMISSIONS = {
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+                || (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                || (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, 101);
+        }
     }
 }
